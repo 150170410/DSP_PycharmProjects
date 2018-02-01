ec2ce8594a9ffea981a02386a8021b2d5ae1207a
debasis <debasis.dash@hpe.com>
2018-02-01 11:51:12 +0530

    Correction done for due import issue in Legacy flow.

--- Dsp-Api-Test-Entel/testsuite/bin/entel_CI_PageObject.py
+++ Dsp-Api-Test-Entel/testsuite/bin/entel_CI_PageObject.py
@@ -7,111 +7,129 @@ config = ConfigParser.RawConfigParser()
 filepath = '..\deployData.txt'
 config.read(filepath)
 
-ftp_ip = config.get('StaticData', 'ftp_ip')
-ftp_uname = config.get('StaticData', 'ftp_uname')
-ftp_pwd = config.get('StaticData', 'ftp_pwd')
-remoteFtp_dir = config.get('StaticData', 'remoteFtp_dir')
-
-local_dir = config.get('StaticData', 'local_dir')
-
-remote_ip = config.get('StaticData', 'remote_ip')
-remote_uname = config.get('StaticData', 'remote_uname')
-remote_pwd = config.get('StaticData', 'remote_pwd')
-remoteSol_dir = config.get('StaticData', 'remoteSol_dir')
-
-imp_sol_path = config.get('StaticData', 'imp_sol_path')
-
-HPSAcheck = '/etc/init.d/activator check'
-HPSAstart = '/etc/init.d/activator start'
-HPSAstop = '/etc/init.d/activator stop'
-importSol = "/opt/OV/ServiceActivator/bin/deploymentmanager  ImportSolution -file /releases/Release_4.6/29012018/TDE/DSP_TDE.zip"
-deleteSol = "/opt/OV/ServiceActivator/bin/deploymentmanager DeleteSolution -solutionName DSP_TDE"
-deploySol = "/opt/OV/ServiceActivator/bin/deploymentmanager DeploySolution -solutionName DSP_TDE -deploymentFile  /opt/OV/ServiceActivator/solutions/DSP_TDE/deploy.xml -dbUser hpsa -dbPassword password"
-undeploySol = "/opt/OV/ServiceActivator/bin/deploymentmanager  UndeploySolution -solutionName DSP_TDE -dbUser hpsa -dbPassword password -dbHost 15.213.50.68 -db ENTELDSP -dbPort 1521"
-startMock = "/home/dspadmin/scripts/checkSaopProcess.sh"
-hpsabinPath = '/opt/OV/ServiceActivator/bin/'
-soldir='/opt/OV/ServiceActivator/solutions/'
-
 
 class CIPageObject(BaseDeployment):
+    ftp_ip = config.get('StaticData', 'ftp_ip')
+    ftp_uname = config.get('StaticData', 'ftp_uname')
+    ftp_pwd = config.get('StaticData', 'ftp_pwd')
+    remoteFtp_dir = config.get('StaticData', 'remoteFtp_dir')
+
+    local_dir = config.get('StaticData', 'local_dir')
+
+    remote_ip = config.get('StaticData', 'remote_ip')
+    remote_uname = config.get('StaticData', 'remote_uname')
+    remote_pwd = config.get('StaticData', 'remote_pwd')
+    remoteSol_dir = config.get('StaticData', 'remoteSol_dir')
+
+    imp_sol_path = config.get('StaticData', 'imp_sol_path')
+
+    HPSAcheck = config.get('StaticData', 'HPSAcheck')
+    HPSAstart = config.get('StaticData', 'HPSAstart')
+    HPSAstop = config.get('StaticData', 'HPSAstop')
+
+    startMock = config.get('StaticData', 'startMock')
+    hpsabinPath = config.get('StaticData', 'hpsabinPath')
+    soldir = config.get('StaticData', 'soldir')
+    importSol_tde = config.get('StaticData', 'importSol_tde')
+    deleteSol_tde = config.get('StaticData', 'deleteSol_tde')
+    deploySol_tde = config.get('StaticData', 'deploySol_tde')
+    undeploySol_tde = config.get('StaticData', 'undeploySol_tde')
+
+    importSol_leg = config.get('StaticData', 'importSol_leg')
+    deleteSol_leg = config.get('StaticData', 'deleteSol_leg')
+    deploySol_leg = config.get('StaticData', 'deploySol_leg')
+    undeploySol_leg = config.get('StaticData', 'undeploySol_leg')
+
+    tde_sol = config.get('StaticData', 'tde_sol')
+    leg_sol = config.get('StaticData', 'leg_sol')
 
     def downloadSolution(self):
-       self.startSSH(ftp_ip,ftp_uname, ftp_pwd)
-       self.sftpGet_data(remoteFtp_dir,local_dir)
+       self.startSSH(self.ftp_ip,self.ftp_uname, self.ftp_pwd)
+       self.sftpGet_data(self.remoteFtp_dir,self.local_dir)
        self.closeSSH()
 
     def uploadSolution(self):
-        self.startSSH(remote_ip,remote_uname,remote_pwd)
-        self.sftpPut_data(local_dir,remoteSol_dir)
+        self.startSSH(self.remote_ip,self.remote_uname,self.remote_pwd)
+        self.sftpPut_data(self.local_dir,self.remoteSol_dir)
         self.closeSSH()
 
     def checkHPSA(self):
-        print(self.sendCommand(HPSAcheck))
-        return self.sendCommand(HPSAcheck)
+        print(self.sendCommand(self.HPSAcheck))
+        return self.sendCommand(self.HPSAcheck)
 
     def startHPSA(self):
         status = self.checkHPSA()
-        if (status.split(' ')[6] == 'not'):
-            print('Starting HPSA$$$$$$$$$$$$$')
-            self.sendCommand(HPSAstart)
-            return True
-        else: return False
+        try:
+            if (status.split(' ')[6] == 'not'):
+                print('Starting HPSA$$$$$$$$$$$$$')
+                self.sendCommand(self.HPSAstart)
+                time.sleep(20)
+                status = self.checkHPSA()
+                if "running" in status.split(' ')[6]:
+                    return True
+            else: return False
+        except AttributeError as e:
+            print(e)
+            return False
     def stopHPSA(self):
         status = self.checkHPSA()
-        if "running" in status.split(' ')[6]:
-            print('Stopping HPSA$$$$$$$$$$$$$')
-            print(self.sendCommand(HPSAstop))
-            time.sleep(10)
-            status = self.checkHPSA()
+        try:
             if "running" in status.split(' ')[6]:
-                print('Stopping HPSA$$$$$$$$$$$$$ Second Time')
-                print(self.sendCommand(HPSAstop))
+                print('Stopping HPSA$$$$$$$$$$$$$')
+                print(self.sendCommand(self.HPSAstop))
                 time.sleep(10)
-            return True
-        elif "not" in status.split(' ')[6]:
-            print('HPSA is not running , thus exiting**')
-            return True
-        else:return False
-
-    def importSolution(self):
+                status = self.checkHPSA()
+                if "running" in status.split(' ')[6]:
+                    print('Stopping HPSA$$$$$$$$$$$$$ Second Time')
+                    print(self.sendCommand(self.HPSAstop))
+                    time.sleep(10)
+                return True
+            elif "not" in status.split(' ')[6]:
+                print('HPSA is not running , thus exiting**')
+                return True
+            else:return False
+        except AttributeError as e:
+            print(e)
+            return False
+    def importSolution(self,importSol,soln):
         print (self.sendCommand(importSol))
         time.sleep(5)
         try:
-            stat= self.sftp.stat(soldir+'DSP_TDE') or self.sftp.stat(soldir+'DSP')
+            stat= self.sftp.stat(self.soldir+soln)
             print('Solution Imported and st_size = ',stat.st_size)
             print('True')
             return True
 
-        except IOError,e:
+        except IOError as e:
             print(e)
             print('False')
             return False
 
-    def deleteSolution(self):
+    def deleteSolution(self,deleteSol,soln):
         print(self.sendCommand(deleteSol))
         time.sleep(5)
         try:
-            stat = self.sftp.stat(soldir + 'DSP_TDE') or self.sftp.stat(soldir + 'DSP')
+            stat = self.sftp.stat(self.soldir + soln)
+            print(deleteSol)
+            print('True')
             return False
 
-        except IOError,e:
+        except IOError as e:
             print(e)
+            print('False')
             return True
 
-    def deploySolution(self):
+    def deploySolution(self,deploySol):
             print(self.sendCommand(deploySol))
             time.sleep(5)
             return True
 
-    def undeploySolution(self):
+    def undeploySolution(self,undeploySol):
             print(self.sendCommand(undeploySol))
             time.sleep(5)
             return True
 
     def startMockservices(self):
-        print(self.sendCommand(startMock))
+        print(self.sendCommand(self.startMock))
         return True
 
-# obj= CIPageObject()
-# obj.startSSH(remote_ip,remote_uname,remote_pwd)
-# obj.importSolution()
